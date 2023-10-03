8966b4c77fd80089369128832364d506911201c0
mk111996 <142637557+mk111996@users.noreply.github.com>
2023-10-03 16:31:02 +0000

    some modifications, will come back some time

--- script.js
+++ script.js
@@ -1,11 +1,62 @@
+const resultNumber = document.querySelector(".operation-result");
+const numberButtons = document.querySelectorAll("#number");
+const operationButtons = document.querySelectorAll("#operation");
+const equalOperation = document.querySelector("#equal");
+const clearOperation = document.querySelector("#clear");
 const turnOn = document.querySelector("#on");
 const turnOff = document.querySelector("#off");
-const operationResult = document.querySelector(".operation-result");
 
-turnOn.addEventListener("click", () => {
-    operationResult.textContent = "0";
+let result = "";
+
+operationButtons.forEach((button) => button.addEventListener("click", operationResult));
+
+equalOperation.addEventListener("click", calculate);
+
+clearOperation.addEventListener("click", clear);
+
+turnOn.addEventListener("click", on);
+
+turnOff.addEventListener("click", off);
+
+numberButtons.forEach((button) => {
+    button.addEventListener("click", displayNumber)
 });
 
-turnOff.addEventListener("click", () => {
-    operationResult.textContent = "";
-});
+function displayNumber() {
+    if (this.textContent === "." && resultNumber.innerHTML.includes(".")) return;
+    if (this.textContent === "." && resultNumber.innerHTML === "") return resultNumber.innerHTML = "0.";
+    resultNumber.innerHTML += this.textContent;
+}
+
+function operationResult() {
+    if (resultNumber.innerHTML === "" && this.textContent ==="-") {
+        resultNumber.innerHTML = "-";
+        return;
+    }
+    else if (resultNumber.innerHTML === "") {
+        return;
+    }
+
+}
+
+function calculate() {
+    let a = Number(resultNumber.innerHTML);
+    let b = Number(resultNumber.innerHTML);
+
+    if (resultNumber.innerHTML === "+") {
+        result = a + b;
+        resultNumber.innerHTML = result;
+    }
+}
+
+function clear() {
+    resultNumber.textContent = "0";
+}
+
+function on() {
+    resultNumber.textContent = "0";
+}
+
+function off() {
+    resultNumber.textContent = "";
+}
